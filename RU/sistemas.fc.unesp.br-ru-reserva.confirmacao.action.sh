f7fea719a3209fc1df3aef173d989de54d46547c
luis-puhl <luispuhl@gmail.com>
2016-06-06 08:34:00 -0300

    [RU] Melhorado aquisição dos dias;

--- RU/sistemas.fc.unesp.br-ru-reserva.confirmacao.action.sh
+++ RU/sistemas.fc.unesp.br-ru-reserva.confirmacao.action.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
+
+build_date() {
+	echo "txt_dias="$1%"2F"$(date +%m)%"2F"$(date +%Y)"&"
+}
+
 reserva_ru() {
-	SEGUNDA="txt_dias=06"%"2F06"%"2F2016&"
-	TERCA="txt_dias=07"%"2F06"%"2F2016&"
-	QUARTA="txt_dias=08"%"2F06"%"2F2016&"
-	QUINTA="txt_dias=09"%"2F06"%"2F2016&"
-	SEXTA="txt_dias=10"%"2F06"%"2F2016"
+	# Week day - today (gets sunday)
+	SUNDAY=$[$(date +%d)-$(date +%w)+7]
+	SEGUNDA=$(build_date $[$SUNDAY+1])
+	TERCA=$(build_date $[$SUNDAY+2])
+	QUARTA=$(build_date $[$SUNDAY+3])
+	QUINTA=$(build_date $[$SUNDAY+4])
+	SEXTA=$(build_date $[$SUNDAY+5])
 	CARTAO=$1
 	CPF=$2
-	DATA="$SEGUNDA$TERCA$QUARTA$QUINTA$SEXTA&txt_cartao=$CARTAO&txt_cpf=$CPF"
+	DATA="$SEGUNDA$TERCA$QUARTA$QUINTA$SEXTA\txt_cartao=$CARTAO&txt_cpf=$CPF"
 	curl "https://sistemas.fc.unesp.br/ru/reserva.confirmacao.action" \
 		-H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" \
 		-H "Referer: https://sistemas.fc.unesp.br/ru/reserva.solicitacao.action" \
@@ -22,7 +29,6 @@ reserva_ru() {
 pesquisa_ru(){
 	DATA="txt_cartao=$1"
 	curl 'https://sistemas.fc.unesp.br/ru/reserva.pesquisar.action' \
-		-H 'Cookie: JSESSIONID=D96695C3E9CD038D930293186900B37D' \
 		-H 'Origin: https://sistemas.fc.unesp.br' \
 		-H 'Accept-Encoding: gzip, deflate' \
 		-H 'Accept-Language: pt-BR,pt;q=0.8,en-US;q=0.6,en;q=0.4' \
