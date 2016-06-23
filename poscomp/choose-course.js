3d51525914668765156b45098aff395acb1c70ac
luis-puhl <luispuhl@gmail.com>
2016-06-23 12:56:10 -0300

    [poscomp] extração das unis funcionando

--- poscomp/choose-course.js
+++ poscomp/choose-course.js
@@ -1,7 +1,8 @@
 const https = require('https');
-const cheerio = require('cheerio');
+// const cheerio = require('cheerio');
 const spawn = require('child_process').spawn;
 
+/*
 function minerarDados(body){
   var $ = cheerio.load(body);
   var scores = $('title')
@@ -41,36 +42,27 @@ function getCallback(response){
     minerarDados(body);
   });
 }
+*/
+
+function turnCURLintoJS(stringCURL) {
+	const regex = new RegExp(" (-H) '([^']+)'| (--data) '([^']+)'| (--compressed)| '([^']+)'|curl");
+	const filterEmpty = function (stringArg) {
+		return stringArg!= undefined && stringArg.length > 0;
+	}
+
+	var split = stringCURL.split(regex);
+	var result = split.filter(filterEmpty);
+
+	return result;
+}
 
 /**
- * to get a curl sample i've copied from chrome and used the folloing regex in relplace from atom:
-
- 	search: " (-H) ('[^']+')| (--data) ('[^']+')| (--compressed)| ('[^']+')"
- 	relplace: "\n"$1",\t"$2",\t"$3",\t"$4",\t"$5",\t"$6","
- then
- 	search: '[\t]?"",'
- 	relplace: ''
+ * @see( exploration_www.fundatec.org.br )
 **/
 function getUniversidades (){
-	// curl 'https://www.fundatec.org.br/portal/concursos/inscricao/padrao/consulta_etapas_ajax.php' -H 'origin: https://www.fundatec.org.br' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: pt-BR,pt;q=0.8,en-US;q=0.6,en;q=0.4' -H 'cookie: _gat=1; __atuvc=2%7C25; __atuvs=576bd13800454b3b000; _ga=GA1.3.527049575.1466523206; __utmt=1; __utma=173084852.527049575.1466523206.1466523247.1466683725.2; __utmb=173084852.1.10.1466683725; __utmc=173084852; __utmz=173084852.1466523247.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)' -H 'pragma: no-cache' -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36' -H 'content-type: application/x-www-form-urlencoded' -H 'accept: */*' -H 'cache-control: no-cache' -H 'authority: www.fundatec.org.br' -H 'referer: https://www.fundatec.org.br/portal/concursos/inscricao/padrao/v20_inscricao_formulario.php' -H 'dnt: 1' --data 'function=1&programa=1&concurso=394' --compressed
+	var stringCURL = "curl 'https://www.fundatec.org.br/portal/concursos/inscricao/padrao/consulta_etapas_ajax.php' -H 'origin: https://www.fundatec.org.br' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: pt-BR,pt;q=0.8,en-US;q=0.6,en;q=0.4' -H 'cookie: _gat=1; __atuvc=3%7C25; __atuvs=576bea8ad06195f7000; _ga=GA1.3.527049575.1466523206; __utmt=1; __utma=173084852.527049575.1466523206.1466683725.1466690682.3; __utmb=173084852.1.10.1466690682; __utmc=173084852; __utmz=173084852.1466523247.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)' -H 'pragma: no-cache' -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36' -H 'content-type: application/x-www-form-urlencoded' -H 'accept: */*' -H 'cache-control: no-cache' -H 'authority: www.fundatec.org.br' -H 'referer: https://www.fundatec.org.br/portal/concursos/inscricao/padrao/v20_inscricao_formulario.php' -H 'dnt: 1' --data 'function=1&programa=1&concurso=394' --compressed";
 
-	let agrv = [
-		"'https://www.fundatec.org.br/portal/concursos/inscricao/padrao/consulta_etapas_ajax.php'",
-		"-H",	"'origin: https://www.fundatec.org.br'",
-		"-H",	"'accept-encoding: gzip, deflate, br'",
-		"-H",	"'accept-language: pt-BR,pt;q=0.8,en-US;q=0.6,en;q=0.4'",
-		"-H",	"'cookie: _gat=1; __atuvc=2%7C25; __atuvs=576bd13800454b3b000; _ga=GA1.3.527049575.1466523206; __utmt=1; __utma=173084852.527049575.1466523206.1466523247.1466683725.2; __utmb=173084852.1.10.1466683725; __utmc=173084852; __utmz=173084852.1466523247.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)'",
-		"-H",	"'pragma: no-cache'",
-		"-H",	"'user-agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'",
-		"-H",	"'content-type: application/x-www-form-urlencoded'",
-		"-H",	"'accept: */*'",
-		"-H",	"'cache-control: no-cache'",
-		"-H",	"'authority: www.fundatec.org.br'",
-		"-H",	"'referer: https://www.fundatec.org.br/portal/concursos/inscricao/padrao/v20_inscricao_formulario.php'",
-		"-H",	"'dnt: 1'",
-		"--data",	"'function=1&programa=1&concurso=394'",
-		"--compressed"
-	];
+	let agrv = turnCURLintoJS(stringCURL);
 
 	const curl = spawn('curl', agrv);
 
@@ -87,28 +79,13 @@ function getUniversidades (){
 	});
 }
 
+/**
+ * @see( exploration_www.fundatec.org.br )
+**/
 function getProgramas( universidadeIndex ) {
-	// curl 'https://www.fundatec.org.br/portal/concursos/inscricao/padrao/consulta_etapas_ajax.php' -H 'origin: https://www.fundatec.org.br' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: pt-BR,pt;q=0.8,en-US;q=0.6,en;q=0.4' -H 'cookie: __atuvc=2%7C25; __atuvs=576bd13800454b3b000; _ga=GA1.3.527049575.1466523206; __utma=173084852.527049575.1466523206.1466523247.1466683725.2; __utmb=173084852.1.10.1466683725; __utmc=173084852; __utmz=173084852.1466523247.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)' -H 'pragma: no-cache' -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36' -H 'content-type: application/x-www-form-urlencoded' -H 'accept: */*' -H 'cache-control: no-cache' -H 'authority: www.fundatec.org.br' -H 'referer: https://www.fundatec.org.br/portal/concursos/inscricao/padrao/v20_inscricao_formulario.php' -H 'dnt: 1' --data 'function=2&instituicao=29&concurso=394&programa=1' --compressed
+	var stringCURL = "curl 'https://www.fundatec.org.br/portal/concursos/inscricao/padrao/consulta_etapas_ajax.php' -H 'origin: https://www.fundatec.org.br' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: pt-BR,pt;q=0.8,en-US;q=0.6,en;q=0.4' -H 'cookie: __atuvc=3%7C25; __atuvs=576bea8ad06195f7000; _ga=GA1.3.527049575.1466523206; __utma=173084852.527049575.1466523206.1466683725.1466690682.3; __utmb=173084852.1.10.1466690682; __utmc=173084852; __utmz=173084852.1466523247.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)' -H 'pragma: no-cache' -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36' -H 'content-type: application/x-www-form-urlencoded' -H 'accept: */*' -H 'cache-control: no-cache' -H 'authority: www.fundatec.org.br' -H 'referer: https://www.fundatec.org.br/portal/concursos/inscricao/padrao/v20_inscricao_formulario.php' -H 'dnt: 1' --data 'function=2&instituicao=65&concurso=394&programa=1' --compressed";
 
-	let argv = [
-		"'https://www.fundatec.org.br/portal/concursos/inscricao/padrao/consulta_etapas_ajax.php'",
-		"-H",	"'origin: https://www.fundatec.org.br'",
-		"-H",	"'accept-encoding: gzip, deflate, br'",
-		"-H",	"'accept-language: pt-BR,pt;q=0.8,en-US;q=0.6,en;q=0.4'",
-		"-H",	"'cookie: __atuvc=2%7C25; __atuvs=576bd13800454b3b000; _ga=GA1.3.527049575.1466523206; __utma=173084852.527049575.1466523206.1466523247.1466683725.2; __utmb=173084852.1.10.1466683725; __utmc=173084852; __utmz=173084852.1466523247.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)'",
-		"-H",	"'pragma: no-cache'",
-		"-H",	"'user-agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'",
-		"-H",	"'content-type: application/x-www-form-urlencoded'",
-		"-H",	"'accept: */*'",
-		"-H",	"'cache-control: no-cache'",
-		"-H",	"'authority: www.fundatec.org.br'",
-		"-H",	"'referer: https://www.fundatec.org.br/portal/concursos/inscricao/padrao/v20_inscricao_formulario.php'",
-		"-H",	"'dnt: 1'",
-		"--data",	"'function=2&instituicao=29&concurso=394&programa=1'",
-		"--compressed"
-	];
-
-	const curl = spawn('curl', agrv);
+	let agrv = turnCURLintoJS(stringCURL);
 
 	curl.stdout.on('data', (data) => {
 	  console.log(`stdout: ${data}`);
@@ -123,7 +100,11 @@ function getProgramas( universidadeIndex ) {
 	});
 }
 
+/*
 https.get('https://news.ycombinator.com/bigrss', getCallback)
   .on('error', function(e){
     console.log("Ocorreu um erro: " + e.message);
   });
+*/
+
+var unis = getUniversidades();
