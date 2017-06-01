ea0c1a248f08a84d4de476740f30c5350ea601bd
Luís Henrique Puhl De Souza <luispuhl@gmail.com>
2017-06-01 11:57:45 -0300

    Update sistemas.unesp.br-sentinela-MessageFix.js

--- sistemas.unesp.br-sentinela-MessageFix.js
+++ sistemas.unesp.br-sentinela-MessageFix.js
@@ -1,6 +1,33 @@
-(function (){
-	$("a[href*='javascript:submitVisualizarMensagem(']").each(function(index, element){
-		var msgId = element.getAttribute('href').replace("javascript:submitVisualizarMensagem('", "").replace("');", "");
-		element.setAttribute('href', "sentinela.viewMessage.action?txt_id=" + msgId + "&emailTipo=recebidas");
-	});
-})();
+/**
+ * On this page 
+ *		https://sistemas.unesp.br/sentinela/common.openMessage.action?emailTipo=recebidas
+ * Trasform this shit
+ *
+
+<a href="javascript:submitVisualizarMensagem('26314345');">
+	<b>Oportunidades e-Deploy</b>
+</a>
+
+ * 
+ * Into this working piece of art
+ *		https://sistemas.unesp.br/sentinela/common.viewMessage.action?txt_id=26314345&emailTipo=recebidas
+ *
+
+<a href="">
+	<b>Oportunidades e-Deploy</b>
+</a>
+
+**/
+let shittyHref = /javascript\:submitVisualizarMensagem/i;
+let lookFor = /javascript:submitVisualizarMensagem\(\'(\d+)\'\);/gi;
+let replaceWith = 'common.viewMessage.action?txt_id=$1&emailTipo=recebidas';
+
+let matches = Array.from( document.getElementsByTagName('a') )
+	.filter(function f(el){
+			return shittyHref.test( el.getAttribute('href') );
+	})
+	.map(function f(el){
+		el.setAttribute('href', el.getAttribute('href').replace(lookFor, replaceWith));
+		return el;
+	 });
+console.log('got ' + matches.length + ' matches');
